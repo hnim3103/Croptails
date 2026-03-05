8e31587f01a48771bc30e88e91a0987740aa1b17
hnim3107 <hnim3107@gmail.com>
2026-03-05 15:00:59 +0700

    feat: create basic house, build simple logic for open and close door, add collision

---
+++ scenes/components/interactable_component.gd
@@ -0,0 +1,16 @@
+class_name InteractableComponent
+extends Area2D
+
+signal interactable_activated
+signal interactable_deactivated
+
+
+	
+
+
+func _on_body_entered(body: Node2D) -> void:
+	interactable_activated.emit()
+
+
+func _on_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	interactable_deactivated.emit()
